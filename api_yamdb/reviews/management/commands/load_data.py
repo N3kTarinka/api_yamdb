dc9f43045688e2cff07bc2087578dcd960d790c1
ilyailyushko <65430523+ilyailyushko@users.noreply.github.com>
2024-09-24 01:21:48 +0300

    Скрипт для заливки данных

--- api_yamdb/reviews/management/commands/load_data.py
+++ api_yamdb/reviews/management/commands/load_data.py
@@ -1,6 +1,7 @@
+from reviews.models import Category, Genre, Title
 import csv
 from django.core.management.base import BaseCommand
-from reviews.models import Category, Genre, Title
+
 
 class Command(BaseCommand):
     help = 'Загрузка данных из CSV файлов в базу данных'
@@ -11,37 +12,35 @@ class Command(BaseCommand):
             reader = csv.DictReader(file)
             for row in reader:
                 Category.objects.get_or_create(
+                    id=row['id'],
                     name=row['name'],
                     slug=row['slug']
                 )
-        self.stdout.write(self.style.SUCCESS('Categories loaded successfully.'))
+        self.stdout.write(self.style.SUCCESS('category.csv загружены.'))
 
         # Загрузка жанров
         with open('static/data/genre.csv', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 Genre.objects.get_or_create(
+                    id=row['id'],
                     name=row['name'],
                     slug=row['slug']
                 )
-        self.stdout.write(self.style.SUCCESS('Genres loaded successfully.'))
+        self.stdout.write(self.style.SUCCESS('genre.csv загружены'))
 
         # Загрузка произведений
         with open('static/data/titles.csv', encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                category = Category.objects.get(slug=row['category'])
-                title, created = Title.objects.get_or_create(
-                    name=row['name'],
-                    year=row['year'],
-                    description=row.get('description', ''),
-                    category=category,
-                )
-                # Если в CSV-файле есть данные о жанрах
-                if row.get('genre'):
-                    genres = row['genre'].split(',')
-                    for genre_slug in genres:
-                        genre = Genre.objects.get(slug=genre_slug)
-                        title.genre.add(genre)
-        self.stdout.write(self.style.SUCCESS('Titles loaded successfully.'))
-
+                try:
+                    category = Category.objects.get(id=row['category'])
+                    Title.objects.get_or_create(
+                        id=row['id'],
+                        name=row['name'],
+                        year=row['year'],
+                        category=category,
+                    )
+                except Category.DoesNotExist:
+                    self.stdout.write(self.style.ERROR(f"Такой категории'{row['category']}' не существует"))
+        self.stdout.write(self.style.SUCCESS('titles.csv загружены'))
