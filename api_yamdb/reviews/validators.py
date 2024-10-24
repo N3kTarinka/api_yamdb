b47d56b248fe92f91058922b531ebcca493a4b42
Никита Жданов <n3ktarinka@yandex.ru>
2024-10-24 23:32:04 +0300

    fixes after the first review

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 
+
 def validate_year(value):
     current_year = timezone.now().year
     if value > current_year:
